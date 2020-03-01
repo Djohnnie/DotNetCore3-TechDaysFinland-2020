e2d8662e5c5fb706a18179838b7e6d598f734849
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2020-03-01 19:55:56 +0100

    Initial commit

---
+++ 07-BuiltInJsonSupport/07-BuiltInJsonSupport/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using FizzWare.NBuilder;
+using Newtonsoft.Json;
+using static System.Console;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace _07_BuiltInJsonSupport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Preparing data...");
+
+            var data = Builder<DataPoint>.CreateListOfSize(10)
+                .All()
+                .With(p => p.Id = Guid.NewGuid())
+                .With(p => p.Title = Faker.Identification.UKNationalInsuranceNumber())
+                .With(p => p.Description = Faker.Lorem.Sentence(100))
+                .With(p => p.LeftData = GenerateDataPoint(0))
+                .With(p => p.RightData = GenerateDataPoint(0))
+                .Build();
+
+            for (int i = 0; i < 3; i++)
+            {
+                var sw1 = Stopwatch.StartNew();
+                string newtonsoftJson = JsonConvert.SerializeObject(data);
+                sw1.Stop();
+                WriteLine($"Newtonsoft JSON serialization:   {sw1.ElapsedMilliseconds}ms");
+
+                sw1.Restart();
+                _ = JsonConvert.DeserializeObject<List<DataPoint>>(newtonsoftJson);
+                sw1.Stop();
+                WriteLine($"Newtonsoft JSON deserialization: {sw1.ElapsedMilliseconds}ms");
+
+                var sw2 = Stopwatch.StartNew();
+                string builtinJson = JsonSerializer.Serialize(data);
+                sw2.Stop();
+                WriteLine($"Built-in JSON serialization:     {sw2.ElapsedMilliseconds}ms");
+
+                sw2.Restart();
+                _ = JsonSerializer.Deserialize<List<DataPoint>>(builtinJson);
+                sw2.Stop();
+                WriteLine($"Built-in JSON deserialization:   {sw2.ElapsedMilliseconds}ms");
+
+                WriteLine();
+            }
+        }
+
+        static DataPoint GenerateDataPoint(int level)
+        {
+            if (level < 10)
+            {
+                return new DataPoint
+                {
+                    Id = Guid.NewGuid(),
+                    Title = Faker.Identification.UKNationalInsuranceNumber(),
+                    Description = Faker.Lorem.Sentence(10),
+                    LeftData = GenerateDataPoint(level + 1),
+                    RightData = GenerateDataPoint(level + 1)
+                };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ 10-WindowsDesktop/10-WindowsDesktop.WPF/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using _10_WindowsDesktop.gRPC.Contract;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace _10_WindowsDesktop.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Point _previousLocation;
+        private Streamer.StreamerClient _client;
+        private AsyncDuplexStreamingCall<StreamRequest, StreamResponse> _duplexStream;
+        private string _clientId = $"{Guid.NewGuid()}";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            _client = new Streamer.StreamerClient(channel);
+
+            _duplexStream = _client.Do(new CallOptions());
+        }
+
+        private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await foreach (var message in _duplexStream.ResponseStream.ReadAllAsync())
+            {
+                _draggableRectangle.SetValue(Canvas.LeftProperty, (double)message.X);
+                _draggableRectangle.SetValue(Canvas.TopProperty, (double)message.Y);
+            }
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var position = e.GetPosition(_draggableRectangle);
+            _previousLocation = position;
+        }
+
+        private async void UIElement_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            var position = e.GetPosition(_draggableRectangle);
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                double xOffset = position.X - _previousLocation.X;
+                double yOffset = position.Y - _previousLocation.Y;
+
+                var left = (double)_draggableRectangle.GetValue(Canvas.LeftProperty);
+                var top = (double)_draggableRectangle.GetValue(Canvas.TopProperty);
+
+                _draggableRectangle.SetValue(Canvas.LeftProperty, left + xOffset);
+                _draggableRectangle.SetValue(Canvas.TopProperty, top + yOffset);
+
+                await _duplexStream.RequestStream.WriteAsync(
+                    new StreamRequest { X = (int)(left + xOffset), Y = (int)(top + yOffset), ClientId = _clientId });
+            }
+        }
+    }
+}
